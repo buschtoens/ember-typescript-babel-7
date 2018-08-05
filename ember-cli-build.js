d2fca97aa42d82f456964d16b9c878149f9fb60c
Jan Buschtöns <jan.buschtoens@clark.de>
2018-08-05 14:08:06 +0200

    chore(build): enable decoratorsBeforeExport

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -11,8 +11,14 @@ module.exports = function(defaults) {
     babel: {
       plugins: [
         '@babel/transform-typescript',
-        ['@babel/plugin-proposal-decorators', { legacy: true }],
-        ['@babel/plugin-proposal-class-properties', { loose: true }],
+        [
+          '@babel/plugin-proposal-decorators',
+          { legacy: true, decoratorsBeforeExport: true }
+        ],
+        [
+          '@babel/plugin-proposal-class-properties',
+          { loose: true }
+        ],
         '@babel/proposal-object-rest-spread'
       ]
     }
