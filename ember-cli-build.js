71b9652513fd715b36d93d010fe03cca3ee1525d
Jan Buschtöns <jan.buschtoens@clark.de>
2018-08-05 13:03:39 +0200

    chore(build): configure Babel 7 for TypeScript

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,18 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   const app = new EmberApp(defaults, {
+    'ember-cli-babel': {
+      includePolyfill: true,
+      extensions: ['js', 'ts']
+    },
+    babel: {
+      plugins: [
+        '@babel/transform-typescript',
+        ['@babel/plugin-proposal-decorators', { legacy: true }],
+        ['@babel/plugin-proposal-class-properties', { loose: true }],
+        '@babel/proposal-object-rest-spread'
+      ]
+    }
   });
 
   return app.toTree();
